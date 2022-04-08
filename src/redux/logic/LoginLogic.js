a28786072dd7e6bddb395018de9b371a66e76af1
abdul247 <abdul.h@chapter247.com>
2022-04-08 19:29:25 +0530

    login issue solve

--- src/redux/logic/LoginLogic.js
+++ src/redux/logic/LoginLogic.js
@@ -7,32 +7,32 @@ import { Toaster } from "../../helper/react-toast";
 const LoginLogic = createLogic({
     type: LoginActionTypes.LOGIN_REQUEST,
     async process(data, dispatch, done) {
-        const { action } = data;
-        let apiResponse = await Axios.post('http://localhost:8080/auth/signin',
-            action.payload)
-            .then((response) => {
-                dispatch(LoginSuccess());
-                Toaster(
-                    {
-                        type: "success",
-                        text: response.data.message
-                    }
-                )
-                const token = response.data.token;
-                localStorage.setItem("token",token)
-                dispatch(push("/dashboard"));
-                done();
-            }
-            ).catch((error) => {
-                dispatch(LoginFailed());
-                Toaster(
-                    {
-                        type: "error",
-                        text: error.response.data.message
-                    }
-                )
-            }
-            )
+      // const { action } = data;
+      // let apiResponse = await Axios.post('http://localhost:8080/auth/signin',
+      //     action.payload)
+      //     .then((response) => {
+      //         dispatch(LoginSuccess());
+      //         Toaster(
+      //             {
+      //                 type: "success",
+      //                 text: response.data.message
+      //             }
+      //         )
+      //         const token = response.data.token;
+      //         localStorage.setItem("token",token)
+      //         dispatch(push("/dashboard"));
+      //         done();
+      //     }
+      //     ).catch((error) => {
+      //         dispatch(LoginFailed());
+      //         Toaster(
+      //             {
+      //                 type: "error",
+      //                 text: error.response.data.message
+      //             }
+      //         )
+      //     }
+      //     )
     },
 });
 
